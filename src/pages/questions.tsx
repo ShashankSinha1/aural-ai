1dad05c5b48b29f388e9bf211070749f561cd85d
victoriadarosa <123495715+victoriadarosa@users.noreply.github.com>
2024-03-23 19:00:05 -0400

    navbar on new pages?

--- src/pages/questions.tsx
+++ src/pages/questions.tsx
@@ -1,8 +1,13 @@
+import "../app/globals.css";
+import Navbar from "../components/Navbar";
+
 const Questions = () => {
     return (
       <div>
-        <h1>Questions Page</h1>
+        <Navbar />
+        <h1 className="text-white-500">Questions Page</h1>
         {/* Add your home page content here */}
+        hello
       </div>
     );
   };
