9b49983c2d8e788d174b05b98f26f18ba2c4413b
victoriadarosa <123495715+victoriadarosa@users.noreply.github.com>
2024-03-23 18:41:00 -0400

    added links

---
+++ src/pages/questions.tsx
@@ -0,0 +1,10 @@
+const Questions = () => {
+    return (
+      <div>
+        <h1>Questions Page</h1>
+        {/* Add your home page content here */}
+      </div>
+    );
+  };
+  
+  export default Questions;
